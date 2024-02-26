21b829f9d4a8f54a1c689441b766b3ab9171b976
xinhnh <xinhkg00@gmail.com>
2024-02-26 23:32:26 +0700

    api like

--- src/components/screen/home/home.js
+++ src/components/screen/home/home.js
@@ -8,12 +8,14 @@ import FriendHome from "./FriendHome"
 import "../../../css/home.css"
 import Popup from 'reactjs-popup';
 import ModalPost from '../post/ModalPost';
+import { LikePost } from '../../../common/callapi/post';
 
 function HomePage(props) {
     const { currUserInfo } = props
     const [showComment, setShowComment] = useState(false)
     const [postcodeState, setPostCode] = useState()
     const [userLogin, setUserLogin] = useState()
+    const [dataLikePost, setDataLikePost] = useState()
     var token = getCookieToken()
     const socket = useContext(SocketContext);
     
@@ -24,7 +26,21 @@ function HomePage(props) {
         setPostCode(getPostcode)
         console.log("vao fnef",getPostcode, postcodeState)
     }
-    console.log("vao fnef222", postcodeState)
+
+    const calApiLikePost = async (postcode) =>{
+        try {
+            const likePostInfo = await LikePost(token, postcode);
+            setDataLikePost(likePostInfo)
+ 
+          } catch (error) {
+            console.error(error)
+          }
+    }
+
+    function handleLikePost(e){
+        var getPostcode =  e.target.attributes.getNamedItem('postcode').value
+        calApiLikePost(getPostcode)
+    }
     useEffect(()=>{
         const dataProfileUser = async () =>{
             try {
@@ -90,14 +106,15 @@ function HomePage(props) {
                                             </div>
                                             <small className="text-muted">Today 7:51 pm</small><br />{/*time real dòng trạng thái*/}
                                             {/*nút like*/}
-                                            <a href="#" className="btn btn-sm btn-danger mt-1 m-1">
-                                                <i className="fa fa-heart-o" /> Like</a>
+                                            <div  onClick={handleLikePost} postcode = "18aad068-3023-47b0-abcb-5deb4028bfc6" className="btn btn-sm btn-danger mt-1 m-1">
+                                                <i className="fa fa-heart-o" /> Like</div>
                                             {/*nút bình luận*/}
-                                            <div onClick={getComments} postcode = "18b5863c-5390-4e26-9e32-2b37fd58b7f8" className="btn btn-sm btn-danger mt-1 m-1"> 
+                                            <div onClick={getComments} postcode = "18aad068-3023-47b0-abcb-5deb4028bfc6" className="btn btn-sm btn-danger mt-1 m-1"> 
                                             <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} fill="currentColor" className="bi bi-chat-dots bi-sm" viewBox="0 0 16 16">
                                                 <path d="M5 8a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm4 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm3 1a1 1 0 1 0 0-2 1 1 0 0 0 0 2z" />
                                                 <path d="M2.165 15.803l.02-.004c1.83-.363 2.948-.842 3.468-1.105A9.06 9.06 0 0 0 8 15c4.418 0 8-3.134 8-7s-3.582-7-8-7-8 3.134-8 7c0 1.76.743 3.37 1.97 4.6a10.437 10.437 0 0 1-.524 2.318l-.003.011a10.722 10.722 0 0 1-.244.637c-.079.186.074.394.273.362a21.673 21.673 0 0 0 .693-.125zm.8-3.108a1 1 0 0 0-.287-.801C1.618 10.83 1 9.468 1 8c0-3.192 3.004-6 7-6s7 2.808 7 6c0 3.193-3.004 6-7 6a8.06 8.06 0 0 1-2.088-.272 1 1 0 0 0-.711.074c-.387.196-1.24.57-2.634.893a10.97 10.97 0 0 0 .398-2z" />
                                             </svg>
+                        
                                             comment
                                             </div>
                                             {/*dòng bình luận*/}
