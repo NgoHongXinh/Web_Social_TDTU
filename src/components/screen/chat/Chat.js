813fb04b9338619ddc4cea9b6ac4c86d09fb1096
xinhnh <xinhnh@seatechit.com.vn>
2024-03-03 14:16:07 +0700

    chat ui

---
+++ src/components/screen/chat/Chat.js
@@ -0,0 +1,15 @@
+function ChatPage(props) {
+    return (
+        <div className="container">
+            <div className="row">
+                <div className="col-sm-4">
+                    Danh sách tin nhắn
+                </div>
+                <div className="col-sm-8">
+                        Khung tin nahanws
+                </div>
+            </div>
+        </div>
+    );
+}
+export default ChatPage
