508c2f88bbd7f72486fa212a5fdab3d6384829b6
xinhnh <xinhnh@seatechit.com.vn>
2024-03-10 02:22:38 +0700

    fix bug

--- src/components/screen/post/ModalPost.js
+++ src/components/screen/post/ModalPost.js
@@ -50,36 +50,46 @@ export default function ModalPost(props) {
   return (
     <div>
         {/* The Modal */}
-        <div className="modal-post-container p-5 border bg-primary mt-1" >
+        <div className="modal-post-container p-5 border bg-custom mt-1" >
             <div className="modal-dialog">
                 <div className="modal-content">
+                    
+                    <div className='modal-post-header-custom'>
+                        <a className='btn btn-custom ' onClick={close}>
+                          <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24" fill="none" stroke="#ffffff" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg>
+                        </a>
+                    </div>
                     {/* Modal Header */}
-                    <div className="modal-header">
-                        <h4 className="modal-title text-white">Tạo bài viết</h4>
+                    <div className="modal-header ">
+                        <h2 className="modal-title ">Tạo bài viết</h2>
+                        
                     </div>
                     {/* Modal body */}
                     <div className="modal-body">
                         <form>
-                            <div className="form-group">
-                                <label htmlFor="recipient-name" className="col-form-label text-white">{userLogin?.data.fullname}</label>
+                            <div className="form-group bg-post-home--custom rounded m-1">
+                                <label htmlFor="recipient-name" className="col-form-label ">{userLogin?.data.fullname}</label>
                                 {/* <input type="text" className="form-control" id="recipient-name" defaultValue="@username" /> */}
                             </div>
                             <div className="form-group">
-                                <label htmlFor="message-text" className="col-form-label text-white">Message:</label>
+                                <label htmlFor="message-text" className="col-form-label">Message:</label>
+                                <div className=' rounded input-post-custom'>
                                 <textarea onChange = {handleInput} className="form-control" id="message-text" defaultValue="" />
+                                </div>
+                                
                             </div>
                             {/*Mục input hình ảnh, file, video*/}
                             <div>
                                 <div className="input-group m-2">
                                     <div>
                                         <i className="bi bi-file-earmark-richtext" />
-                                        <label htmlFor="image_uploads" className='text-white'>Choose images to upload (PNG, JPG, JPEG)</label>
+                                        <label htmlFor="image_uploads" className='p-1'>Choose images to upload (PNG, JPG, JPEG)</label>
                                         {/* <input hidden onChange={onImageChange} type='file' id='input-img' accept='image/*'></input> */}
-                                        <input onChange={changeImage} className="fa fa-image icon text-white" type="file" id="image_uploads" name="image_uploads" accept=".jpg, .jpeg, .png" multiple aria-hidden="true" />
+                                        <input onChange={changeImage} className="btn rounded btn-custom p-2" type="file" id="image_uploads" name="image_uploads" accept=".jpg, .jpeg, .png" multiple aria-hidden="true" />
                                     </div>
                                     <div>
-                                        <label htmlFor="file_uploads" className='text-white'>Choose video to upload</label>
-                                        <input  onChange={changeVideo} className="fa fa-file icon text-white" type="file" id="file_uploads" aria-hidden="true" />
+                                        <label htmlFor="file_uploads" className='p-1'>Choose video to upload</label>
+                                        <input  onChange={changeVideo} className="fa fa-file icon " type="file" id="file_uploads" aria-hidden="true" />
 
                                     </div>
                                     <div className="preview">
@@ -88,8 +98,7 @@ export default function ModalPost(props) {
                             </div></form>
                         {/* Modal footer */}
                         <div className="modal-footer">
-                            <button type="button" className="btn btn-secondary m-1" data-dismiss="modal"  onClick={close}>Close</button>
-                            <button type="button" onClick={createNewPost} className="btn btn-danger m-1">Posted</button>
+                            <button type="button" onClick={createNewPost} className="btn btn-danger mt-5 btn-lg w-100">Posted</button>
                         </div>
                     </div>
                 </div>
