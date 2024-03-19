ae42051fb0bb26a63b26abe28792436ed1d6b110
xinhnh <xinhkg00@gmail.com>
2024-03-19 22:45:45 +0700

    hiển thị ảnh user chat

--- src/components/screen/chat/chat_page.js
+++ src/components/screen/chat/chat_page.js
@@ -330,7 +330,7 @@ function ChatPage() {
 
                     <div className="chat-message-right p-4">
                     <div>
-                        <img src={userLogin['picture']}  className="rounded-circle mr-1" alt="Chris Wood" width={40} height={40} />
+                        <img src={userLogin['data']['picture']}  className="rounded-circle mr-1" alt="Chris Wood" width={40} height={40} />
                         {/* <div className="text-muted small text-nowrap mt-2">2:33 am</div> */}
                     </div>
                     <div className="flex-shrink-1 bg-custom text-white rounded py-2 px-3 mr-3 mess-text-content">
